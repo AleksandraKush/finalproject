9c63e4775439c7a53a1496f6df6b9e51a7c06fbd
AleksandraKush <sashastav26@gmail.com>
2022-12-27 19:51:47 +0300

    add method FindArray

--- Program.cs
+++ Program.cs
@@ -9,19 +9,40 @@
     return array;
 }
 
-void PrintArray(string[] array)
+void PrintArray(string[] arr)
 {
     Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-      if(i < array.Length - 1) Console.Write($"{array[i]}, ");
-      else Console.Write($"{array[i]}");
+      if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+      else Console.Write($"{arr[i]}");
     }
     Console.WriteLine("]");
 }
 
+string[] FindArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) count ++;
+    }
+    string[] newArr = new string[count];
+    int k = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (arr[i].Length <= 3)
+       {
+         newArr[k] = arr[i];
+         k++;
+       }
+    }
+    return newArr;
+}
 
 Console.WriteLine("введите количество элементов, которое собираетесь вводить");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] array4 = GetArray(size);
 Console.WriteLine();
+string[] newArray = FindArray(array4);
+PrintArray(newArray);
