0a8eac64002acf53ca0212fdb2cd097b60f7513c
AleksandraKush <sashastav26@gmail.com>
2022-12-27 19:44:47 +0300

    add method GetArray

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] GetArray(int num)
+{
+    string[] array = new string [num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("введите элемент");
+        array[i] = (Convert.ToString(Console.ReadLine()));
+    }
+    return array;
+}
+
+Console.WriteLine("введите количество элементов, которое собираетесь вводить");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array4 = GetArray(size);
+Console.WriteLine();
