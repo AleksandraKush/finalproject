2e333f6adf9568c4e95f5250afa5f7e824ec38a1
AleksandraKush <sashastav26@gmail.com>
2022-12-27 19:48:36 +0300

    add method PrintArray

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@
     return array;
 }
 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+      if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+      else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
 Console.WriteLine("введите количество элементов, которое собираетесь вводить");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] array4 = GetArray(size);
